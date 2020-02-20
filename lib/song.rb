a98023710fcbc88e255a8b3510697268123434a6
Mark Drehmann <markdrehmann@gmail.com>
2020-02-20 18:21:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   def self.new_from_filename(filename)
     data = filename.chomp(".mp3").split(" - ")
     song = self.new
-    song.artist = data[0]
+    song.artist_name = data[0]
     song.name = data[1]
     song
   end
