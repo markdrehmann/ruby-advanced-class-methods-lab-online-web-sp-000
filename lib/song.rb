cbb22bf7c46c1d5633a8be16ad0db2b7021a77ae
Mark Drehmann <markdrehmann@gmail.com>
2020-02-20 18:03:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,27 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    search = @@all.find {|song| song.name == name}
+    search
+  end
+
 end
